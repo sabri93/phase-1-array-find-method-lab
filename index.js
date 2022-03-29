70279667ddf304f6ff7ccf0dfea7e18767497550
sabrina <sabrinadjebbar93@gmail.com>
2022-03-28 20:35:06 -0400

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+ function superbowlWin(record){
+    function yearFind(year){
+    
+        return year.result==="W"
+      
+    }
+    const winYear = record.find(yearFind)
+    if (winYear){
+        return winYear.year
+    }
+       
+        
+}
+      
+
+    
+    
+    
+ 
+
+ 
+ 
+
+
+
+ 
+
+ 
+
+
